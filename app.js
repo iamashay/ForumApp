49e9942a09d999b00874f3fffc4462612462ca5b
iamashay <ashaywin@gmail.com>
2023-11-14 00:03:28 +0530

    Complete hashed sign up page

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const passport = require('authentication.js')
 require('dotenv').config()
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
+
 const mongoDB = process.env.MONGO_URL;
 const app = express();
 const indexRouter = require('./routes/index')
@@ -15,8 +17,9 @@ async function main() {
 main().catch((err) => console.log(err));
 
 // view engine setup
-app.set('views', './views');
+app.set('views', './views/');
 app.set('view engine', 'ejs');
+app.set('env', 'development');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -33,12 +36,12 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  //res.locals.message = err.message;
+  //res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.send('error');
+  req.app.get('env') === 'development' ? res.send(`<pre>${err.stack}</pre>`) : res.send('Error!');
 });
 
 app.listen(process.env.port || 3000, ()=> console.log(`Server listening at http://localhost:${process.env.port}`))
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,53 @@
 const express = require('express')
 const router = express.Router()
+const { body, validationResult } = require("express-validator");
+const UserModel = require('../models/User')
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 router.get('/', function(req, res){ 
-    res.send('asd')
+    res.render('home', {user: false, posts: false})
 })
+router.get('/signin', function(req, res){ 
+    res.render('signin', {user: false, errors: false, username: false})
+})
+router.post('/signin', function(req, res){ 
+    passport.authenticate("local", res.redirect('/'))
+})
+router.get('/signup', function(req, res){ 
+    res.render('signup', {user: false, errors: false, username: false, success: false})
+})
+router.post('/signup',   
 
+    body("username", "Title must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("username").custom(async (username) => {
+        const user = await UserModel.findOne({username})
+        if (user) {
+            throw new Error('Username is already in use')
+        }
+    }),
+    body("password", "Password must be of at least 4 characters.")
+        .trim()
+        .isLength({ min: 4 })
+        .escape(),  
+    async function(req, res){ 
+        const {username, password} = req.body
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            console.log(errors.array())
+            res.render('signup', {user: false, username, errors: errors.array(), success: false})
+            return
+        }
+        const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+        const User = new UserModel({
+            username,
+            password: hashedPassword
+        })
+        await User.save()
+        res.render('signup', {user: false, username: false, errors: false, success: 'Successully signed up!'})
+
+})
 module.exports = router
