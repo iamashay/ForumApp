f661e4120dabe052cdadc15020e059e91c157de2
iamashay <ashaywin@gmail.com>
2023-11-18 16:23:31 +0530

    fix some errors related to authentication

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 const UserSchema = new Schema({
     username: {type: String, required: true, index: {unique: true}},
     password: {type: String, required: true},
+    googleId: {type: String},
     role: {type: String, default: 'user'},
 }, {
     timestamps: true,
